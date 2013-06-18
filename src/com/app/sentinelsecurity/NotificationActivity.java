724ce6cc6297aaecab83c05109ebe2d01f82b095
Jason Hamm <mhamm@availity.com>
2013-06-18 05:52:32 -0400

    New activity to handle notifications (form section)

---
+++ src/com/app/sentinelsecurity/NotificationActivity.java
@@ -0,0 +1,58 @@
+package com.app.sentinelsecurity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.app.sentinelsecurity.domain.Question;
+
+public class NotificationActivity extends Activity {
+	Context context = this;
+	QuestionsAdapter adapter;
+	List<Question> questions;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		questions = createQuestions();
+		setContentView(R.layout.activity_notification);
+
+		adapter = new QuestionsAdapter(context, questions);
+		ListView notifications = (ListView) findViewById(R.id.list_notification);
+
+		Button next = (Button) findViewById(R.id.button_notification_next);
+		next.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			}
+		});
+
+		Button back = (Button) findViewById(R.id.button_notification_back);
+		back.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			}
+		});
+	}
+
+	private List<Question> createQuestions() {
+		List<Question> questions = new ArrayList<Question>();
+		Question question1 = new Question();
+		question1.setQuestion(getResources().getString(R.string.notification_1));
+		questions.add(question1);
+		Question question2 = new Question();
+		question2.setQuestion(getResources().getString(R.string.notification_2));
+		questions.add(question2);
+		Question question3 = new Question();
+		question3.setQuestion(getResources().getString(R.string.notification_3));
+		questions.add(question3);
+		return questions;
+	}
+
+}
